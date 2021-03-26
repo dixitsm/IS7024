b544a30c9be3748526834fc8f6c49f5848afa1f0
dixitsm <dixitsm@mail.uc.edu>
2021-03-26 18:09:16 -0400

    Moderna Vaccine distribution data API  config

--- Covinator/Covinator/Pages/Index.cshtml.cs
+++ Covinator/Covinator/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+
+
 
 namespace Covinator.Pages
 {
@@ -12,14 +16,23 @@ namespace Covinator.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+
+
         public void OnGet()
         {
-
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://data.cdc.gov/resource/b7pe-5nws.json");
+                var modernaVaccineDistributionAllocations = ModernaVaccineDistributionAllocations.FromJson(jsonString);
+                ViewData["ModernaVaccineDistributionAllocations"] = modernaVaccineDistributionAllocations;
+            }
         }
     }
 }
