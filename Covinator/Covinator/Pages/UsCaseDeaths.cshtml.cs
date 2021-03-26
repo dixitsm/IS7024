4d50fe818451844ea45b7ae7bca89b4ecc5f41b2
sapravl <77544569+sapravl@users.noreply.github.com>
2021-03-26 13:07:17 -0400

    UsCaseDeaths Data
    This data has information on all the active Covid Cases and deaths in US by State.

--- Covinator/Covinator/Pages/UsCaseDeaths.cshtml.cs
+++ Covinator/Covinator/Pages/UsCaseDeaths.cshtml.cs
@@ -1,24 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using QuickType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickType;
 
 namespace Covinator.Pages
 {
-    public class IndexModel : PageModel
+    public class UsCaseDeathsModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
             using (var webClient = new WebClient())
@@ -27,7 +19,6 @@ namespace Covinator.Pages
                 var casesDeaths = CasesDeaths.FromJson(jsonString);
                 ViewData["CasesDeaths"] = casesDeaths;
             }
-                
         }
     }
 }
